4f4707731244bc26286833152e80ecd8d95dc0f2
liuxm <liuxm@AnniedeMacBook-Pro.local>
2017-09-08 13:01:19 +0800

    第一次添加文件

---
+++ src/main/java/com/evan/springboot/chapter1/Main.java
@@ -0,0 +1,20 @@
+package com.evan.springboot.chapter1;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * Created by liuxm on 2017/9/8.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // 使用AnnotationConfigApplicationContext作为Spring容器，接受输入一个配置类作为参数
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(FunctionConfig.class);
+        // 获得声明配置的UseFunctionService得Bean
+        UseFunctionService useFunctionService =  context.getBean(UseFunctionService.class);
+
+        System.out.printf(useFunctionService.SayHello("word"));
+
+        context.close();
+    }
+}
+
