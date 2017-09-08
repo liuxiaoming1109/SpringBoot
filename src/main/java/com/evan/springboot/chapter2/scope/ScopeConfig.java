42c58c09b844f3eaf7d170b44a7929475b61beb0
liuxm <liuxm@AnniedeMacBook-Pro.local>
2017-09-08 17:40:43 +0800

    添加Scope

---
+++ src/main/java/com/evan/springboot/chapter2/scope/ScopeConfig.java
@@ -0,0 +1,12 @@
+package com.evan.springboot.chapter2.scope;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by liuxm on 2017/9/8.
+ */
+@Configuration
+@ComponentScan("com.evan.springboot.chapter2.scope")
+public class ScopeConfig {
+}
